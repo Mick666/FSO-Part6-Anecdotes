7ff5428b7c2ed7b600a3ba266bb59bbe55325606
Michael Martino <Mick666@hotmail.com.au>
2020-09-02 23:20:27 +1000

    Exercises 6.7, 6.8

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { addVote, createAnecdote } from './reducers/anecdoteReducer'
+import { addVote } from './reducers/anecdoteReducer'
+import NewAnecdote from './components/NewAnecdote'
+import Anecdotes from './components/Anecdotes'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
-      event.preventDefault()
-      const content = event.target.anecdote.value
-      event.target.anecdote.value = ''
-      dispatch(createAnecdote(content))
-  }
-
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(addVote(anecdote.id))}>vote</button>
-          </div>
+    return (
+        <div>
+            <Anecdotes />
+            <NewAnecdote />
         </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <input name="anecdote" /> 
-        <button type="submit">add</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default App
